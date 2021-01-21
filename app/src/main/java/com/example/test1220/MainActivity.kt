7d80977873a61eac9e08ad184c185faec7ad017c
Jane <jane.was.gentleman@gmail.com>
2021-01-21 21:11:55 +0900

    fix SubActivity
    fix SubActiity Bugs.
    
    add InAppWebViewButton
    add browserButton

--- app/src/main/java/com/example/test1220/MainActivity.kt
+++ app/src/main/java/com/example/test1220/MainActivity.kt
@@ -1,13 +1,39 @@
 package com.example.test1220
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val inAppWebViewButton = findViewById<Button>(R.id.inAppWebViewButton)
+        val browserButton = findViewById<Button>(R.id.browserButton)
+        inAppWebViewButton.setOnClickListener {
+            moveSubActivity()
+        }
+        browserButton.setOnClickListener {
+            showBrowser()
+        }
+    }
+
+    private fun moveSubActivity() {
+        val intent = Intent(this, SubActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun showBrowser() {
+        val uri = Uri.parse(BROWSER_URL)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    companion object {
+        private const val BROWSER_URL = "https://test-webviewapp01.jimdofree.com/%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9/"
     }
 }
--- app/src/main/java/com/example/test1220/SubActivity.kt
+++ app/src/main/java/com/example/test1220/SubActivity.kt
@@ -1,10 +1,10 @@
 package com.example.test1220
 
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
 import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
 
-class SubActivity : WearableActivity() {
+class SubActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,11 +12,8 @@ class SubActivity : WearableActivity() {
 
         // webView
         val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.settings.javaScriptEnabled = true//JavaScript を有効にする
+        myWebView.settings.javaScriptEnabled = true //JavaScript を有効にする
         myWebView.loadUrl("https://test-webviewapp01.jimdofree.com/")
         // webView END
-
-        // Enables Always-on
-        setAmbientEnabled()
     }
 }
