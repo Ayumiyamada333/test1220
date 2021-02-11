6c20fcb6c0148da22067f260329789e15bde1586
yamada3345 <p.a333a.i@gmail.com>
2021-02-11 19:07:00 +0900

    subactivitty追加

--- app/src/main/java/com/example/test1220/MainActivity.kt
+++ app/src/main/java/com/example/test1220/MainActivity.kt
@@ -5,8 +5,13 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.logEvent
+import kotlinx.android.synthetic.main.activity_main.* //?
 
 class MainActivity : AppCompatActivity() {
+    private val sendEventHelper by lazy { SendEventHelper(this) } //SendEventHelper呼び出し
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +23,11 @@ class MainActivity : AppCompatActivity() {
         }
         browserButton.setOnClickListener {
             showBrowser()
+        }
+        //ユーザー ID を設定する
+        FirebaseAnalytics.getInstance(this).setUserId("aaaaa")
+        FirebaseAnalytics.getInstance(this).logEvent("eventname") {
+            param("param_user_id", "p_aaaaa")
         }
     }
 
@@ -35,5 +45,27 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val BROWSER_URL = "https://test-webviewapp01.jimdofree.com/%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9/"
+    }
+
+    //clickイベント計測
+    private fun initialize() {
+        initLayout()
+    }
+
+    private fun initLayout() {
+        initClick()
+    }
+
+    //private fun　削除？
+    //inAppWebViewButton　削除？
+    //sendEventHelper　削除？
+    private fun initClick() {
+        inAppWebViewButton.setOnClickListener {
+            sendEventHelper.sendSignUpEvent("アプリ内で開く")
+            FirebaseAnalytics.getInstance(this).logEvent(FirebaseAnalytics.Event.VALUE) {
+                Param(FirebaseAnalytics.Param.VALUE,"アプリ内で開く")
+            }
+        }
+
     }
 }
